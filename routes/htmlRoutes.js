c41a489fc3308a730393e2043e8d388464c89379
Rebecca Hom <rebecca.hom001@gmail.com>
2019-01-12 11:02:52 -0800

    added paths to other static pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,17 @@
 var path = require("path");
 
 module.exports = (app) => {
+    // Shows table and waiting list.
+    app.get("/tables", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "tables.html"));
+    });
+
+    // Shows reservation form.
+    app.get("/reserve", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "reserve.html"));
+    });
+
+    // Defaults to home page.
     app.use(function(req, res) {
         res.sendFile(path.join(__dirname, "../public", "home.html"));
     });
