c4dd8b60d93438321ad4c918b6a9489080fc2b43
Rebecca Hom <rebecca.hom001@gmail.com>
2019-01-12 10:57:04 -0800

    added express dependency

--- server.js
+++ server.js
@@ -6,10 +6,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-//==========================================================================
+//=========================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for an express server
-//==========================================================================
+//=========================================================================
 
 //Tells node that we are creating an "express" server
 var app = express();
@@ -21,18 +21,18 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//==========================================================================
-ROUTER
-//  The below points our server to a series of "route" files.
+//=========================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-//==========================================================================
+//=========================================================================
 
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-//==========================================================================
+//=========================================================================
 //The below code effectively "starts" our server
-//==========================================================================
+//=========================================================================
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: "+ PORT);
